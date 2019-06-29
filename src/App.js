8fee2baf4ff47028f8ec60153a5c2c98ac46f7a0
pson0007 <ping.song@monash.edu>
2019-06-29 14:19:07 +1000

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Container from "./components/Container"
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Navigation />
         <Container />
