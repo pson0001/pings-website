884235867eaff6a9c7307a26350454973fbf110b
pson0007 <ping.song@monash.edu>
2019-06-29 14:20:35 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pings-website/precache-manifest.96ca67d5ca8bf2f83d70f4a97990515b.js"
+  "/pings-website/precache-manifest.01786e33fb6c706e5723f68235a3ec4c.js"
 );
 
 workbox.clientsClaim();
